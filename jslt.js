d91dde21bbb8d1f29074468ba7d61392b9192f01
DicanGuo2023 <128370400+DicanGuo2023@users.noreply.github.com>
2023-03-23 08:56:58 +1000

    Update jslt.js
    update the code using pipeline & hash-int.

--- jslt.js
+++ jslt.js
@@ -2,48 +2,50 @@ let hardCodedValues = {
   "RegisteredOfficeAddress": {
     "addressCategoryType": "G",
     "addressTypeCode": 162, 
-    "oidDigest":  "1143470109"  },
+  },
   "PlaceOfBusiness": {
     "addressCategoryType": "G",
     "addressTypeCode": 164, 
-    "oidDigest":  "457686347"  },
+  },
   "EmailAddress": {
     "addressCategoryType": "E",
     "addressTypeCode": 170, 
-    "oidDigest":  "-487739423"  },
+  },
   "MobileAddress": {
     "addressCategoryType": "T",
     "addressTypeCode": 122, 
-    "oidDigest":  "-1415936973"  }
+  }
 }
-def convert(array)
-  if($array)
-    let type = $array.name    
+def convert(array) $array|
+  if(.)
+    let type = .name    
     let extraInfo = get-key($hardCodedValues, $type)
+    let addressesAttributes = .addresses.attributes
     {
-      "id": $array.id,
-      "type": $array.name,
+      "id": .id,
+      "type": .name,
       "attributes": {
         "addressCategoryType": $extraInfo.addressCategoryType,
         "addressTypeCode": $extraInfo.addressTypeCode,
-        "localityName": $array.addresses.attributes.City,
-        "addressPostcode": $array.addresses.attributes.PostCode,
-        "addressStateCode": $array.addresses.attributes.RegionCode,
-        "addressCountryCode": $array.addresses.attributes.CountryCode,
-        "addressLine1": $array.addresses.attributes.Line1,
-        "addressLine2": $array.addresses.attributes.Line2,
+        "localityName": $addressesAttributes .City,
+        "addressPostcode": $addressesAttributes.PostCode,
+        "addressStateCode": $addressesAttributes.RegionCode,
+        "addressCountryCode": $addressesAttributes.CountryCode,
+        "addressLine1": $addressesAttributes.Line1,
+        "addressLine2": $addressesAttributes.Line2,
         // email& mobile attributes        
-        "internetEmailAddress": $array.attributes.Email,
-        "phoneNumber": $array.attributes.Number,
-        "countryCallingCode": $array.attributes.CountryCallingCode,
-        "startDate": $array.attributes.StartDate      },
-      "oidDigest": $extraInfo.oidDigest,
+        "internetEmailAddress": .attributes.Email,
+        "phoneNumber": .attributes.Number,
+        "countryCallingCode": .attributes.CountryCallingCode,
+        "startDate": .attributes.StartDate      
+      },
+      "oidDigest": hash-int(.)
     }
-// looping through all items
+
 let targetAddress = [for(.)
-  // if it is an array of addresses  
   if(is-array(.value)) 
     [for(.value) convert(.)]
   else convert(.value)
 ]
+
 { "addresses":  flatten($targetAddress)  }
